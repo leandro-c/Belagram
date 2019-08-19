29bfe046d35625bc59f4232324d1092500526fa2
Leandro Cesar Casarín <lcasarin@belatrix.com>
2019-08-19 10:33:26 -0300

    fixid call to api

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,20 +1,67 @@
 import React, { useEffect, useState } from 'react'
 import { Category } from '../Category'
-import { List, Item } from './styles'
-
-export const ListOfCategories = () => {
+import { List, Item, Spinner } from './styles'
+// Custom Hook
+const useCategoriesData = () => {
   const [categories, setCategories] = useState([])
+  const [loading, setLoading] = useState(false)
+  // uno para recuperar los datos de las categorias
   useEffect(function () {
-    window.fetch('https://belagram.leandroccasarin.now.sh/categories')
+    setLoading(true)
+    window.fetch('https://belagram-server.leandroccasarin.now.sh/categories')
       .then(res => res.json())
       .then(res => {
         setCategories(res)
+        setLoading(false)
       }, [])
   })
-  return (
-    <List>
-      { categories.map(category =>
-        <Item key={category.id}><Category {...category} /></Item>)}
+  return { categories, loading }
+}
+export const ListOfCategories = () => {
+  const { categories, loading } = useCategoriesData()
+  const [showFixed, setShowFixed] = useState(false)
+
+  // otro para mostrar las categorias fijas cuando haga falta
+  /* useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return function () {
+      document.removeEventListener('scroll', onScroll)
+    }
+  }, [showFixed]) */
+
+  useEffect(function () {
+    function onScroll (e) {
+      const isShowFixed = window.scrollY > 200
+      setShowFixed(isShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return function () {
+      document.removeEventListener('scroll', onScroll)
+    }
+  }, [showFixed])
+
+  // puedo usar mas de un useEffect
+  const renderList = (fixed) => (
+    <List fixed={fixed}>
+      {
+        categories.map(category =>
+          <Item key={category.id}><Category {...category} /></Item>)
+      }
     </List>
+  )
+  // if (loading) return <Spinner />
+  return (
+    <React.Fragment>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </React.Fragment>
   )
 }
--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -1,9 +1,46 @@
-import styled from 'styled-components'
+import styled, { css, keyframes } from 'styled-components'
 
 export const List = styled.ul`
     display: flex;
-    overflow:scroll;
+    overflow:scroll; 
     width:100%;
+    ${props => props.fixed && css`
+        background: #fff;
+        border-radius:60px;
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+        left:0;
+        margin: 0 auto;
+        max-width:400px;
+        padding: 5px;
+        position: fixed;
+        right: 0;
+        top: -20px;
+        transform: scale(.5);
+        z-index: 1;
+    `}
+`
+const rotateFrames = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`
+
+export const rotate = () => css`
+  animation: 1.5s ${rotateFrames} linear infinite;
+`
+export const Spinner = styled.div`
+  ${rotate()}
+  border:2px solid violet;
+  border-right: 2px solid transparent;
+  border-radius: 50%;
+  height:40px;
+  margin-bottom: 10px;
+  margin-left: calc(50% - 40px);
+  width:40px;
+  z-index: 10;
 `
 
 export const Item = styled.li`
